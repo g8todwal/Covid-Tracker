2b8c342acf058b04accd8ae271d05f47b3272122
g8todwal <g8todwal@gmail.com>
2020-08-30 12:37:28 +0530

    mbile-draft

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -9,7 +9,7 @@ const InfoBox = ({ title, cases, isRed, active, total, ...props }) => {
 					${isRed && 'infoBox-red'}`}>
 			<CardContent>
 				<Typography className="infoBox-title" color="textSecondary">
-					{title}
+					<strong>{title}</strong>
 				</Typography>
 				<h2 className={`infoBox-cases ${!isRed && "cases-green"}`}>{cases}</h2>
 				<Typography className="infoBox-total" color="textSecondary">
